db7f56fa45d34b3fe1e77414f645f1ec920ee57d
shubhendra-20 <37156857+shubhendra-20@users.noreply.github.com>
2019-02-15 22:44:38 +0530

    The last digit

---
+++ Lastdig.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+  int main(void) {
+
+long long int a,p,q,b;
+
+int t;
+
+scanf("%d",&t);
+
+while(t--)   {
+
+scanf("%lld %lld",&a,&b);
+
+p=a%10;
+
+q=b%4;
+
+if(b==0)
+
+    printf("1\n");
+
+else if(p==1||p==0||p==5||p==6)
+
+printf("%d\n",p);
+
+else if(q==1)
+
+    printf("%d\n",p);
+
+else if(q==2)
+
+    printf("%d\n",((p*p)%10));
+
+else if(q==3)
+
+    printf("%d\n",((p*p*p)%10));
+
+else if(q==0)
+
+    printf("%d\n",((p*p*p*p)%10));
+
+}
+
+return 0;
+
+}

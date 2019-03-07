31bc4bd1e843fbaddfa4b6c658c2191304e24ad9
shubhendra-20 <37156857+shubhendra-20@users.noreply.github.com>
2019-03-07 22:56:23 +0530

    Create onp.cpp

---
+++ onp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <cctype>
+#include <cstring>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    char str[1000];
+    stack <char> s;
+    while (t--) 
+    {
+        cin>>str;
+        int len = strlen (str);
+
+        for(int i=0;i<len;i++ ) 
+        {
+            if(isalpha(str[i]))
+                cout << str[i];
+            else if(str[i] == ')' ) 
+            {
+                cout << s.top ();
+                s.pop ();
+            }
+
+            else if (str[i] != '(' )
+                s.push (str[i]);
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
